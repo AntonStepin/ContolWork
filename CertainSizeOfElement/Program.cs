d0f49287763fc436e725f883661071c861b2dba5
Anton Stepin <ooosg@rambler.ru>
2022-06-30 22:55:33 +0300

    CertainSizeOfElement v1 full

--- CertainSizeOfElement/Program.cs
+++ CertainSizeOfElement/Program.cs
@@ -1,1 +1,51 @@
 ﻿
+string[] CertainSizeOfElement(string[] InitialArray, int SizeOfElement)
+{
+    int NumberOfElements = 0;
+    for (int i = 0; i < InitialArray.Length; i++)
+    {
+        if (InitialArray[i].Length <= SizeOfElement)
+        {
+            NumberOfElements++;
+        }
+
+    }
+    string[] ResultArray = new string[NumberOfElements];
+    int ElementRA = 0;
+    for (int i = 0; i < InitialArray.Length; i++)
+    {
+        if (InitialArray[i].Length <= SizeOfElement)
+        {
+            ResultArray[ElementRA] = InitialArray[i];
+            ElementRA++;
+        }
+    }
+    return ResultArray;
+}
+
+
+void PrintArray(string[] Array)
+{
+    Console.Write("[");
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i]);
+        if (i < Array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+string[] InitialArray =
+{
+    "hello",
+    "2",
+    "world",
+    ":-)"
+};
+int SizeOfElement = 3;
+string[] ArrayCertainElements = CertainSizeOfElement(InitialArray, SizeOfElement);
+PrintArray(ArrayCertainElements);
